992f73153ea0df6b8c3e680396b20912e486d80d
Jaden Plugowsky <124389316+Jaden-Plugowsky@users.noreply.github.com>
2023-04-10 17:53:29 +0000

    final website

--- js/script.js
+++ js/script.js
@@ -1,13 +1,22 @@
 // Copyright (c) 2023 Jaden Plugowsky All rights reserved
 //
 // Created by: Jaden Plugowsky
-// Created on: March 2023
+// Created on: April 2023
 // This file contains the JS functions for index.html
 
 "use strict"
 
-function buttonOneClicked() {
-  alert("Hello, World!")
+function calculatePressed() {
+  //This function calculates the area of a trapezoid
+  //Input through Text Fields
+  const baseOne = parseFloat(document.getElementById("base-one").value)
+  const baseTwo = parseFloat(document.getElementById("base-two").value)
+  const height = parseFloat(document.getElementById("height").value)
+
+  //Process
+  const area = ((baseOne + baseTwo) / 2) * height
+
+  //Output
   document.getElementById("answer").innerHTML =
-    "The answer is: " + "insert answer here"
+    "The area of your Trapezoid is: " + area + "cm²"
 }
